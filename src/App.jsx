d3afed8d0869bf4f389602bfea98b233d28fa69b
Fathima-Mufeeda-Muhammed <mufeedamuhammed1099@gmail.com>
2024-11-01 15:24:51 +0530

    commit1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import ThemeContextProvider from "./context/ThemeContextProvider";
+
+function App() {
+  return (
+    <ThemeContextProvider>
+      <div className="flex">
+        <Sidebar />
+        <div className="grow ml-16 md:ml-64 h-full lg:h-screen border-r-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white">
+          <Navbar />
+          <div>
+            <Dashboard />
+          </div>
+        </div>
+      </div>
+    </ThemeContextProvider>
+  );
+}
+
+export default App;
